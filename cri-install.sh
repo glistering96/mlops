6c79ce6ff501ca0ccb1f1e8ff7ff0b0298dcbcc7
wonjun <glistering96@gmail.com>
2025-01-29 00:17:54 +0900

    cri dockered

---
+++ cri-install.sh
@@ -0,0 +1,50 @@
+# cri-docker Install
+VER=$(curl -s https://api.github.com/repos/Mirantis/cri-dockerd/releases/latest|grep tag_name | cut -d '"' -f 4|sed 's/v//g')
+echo $VER
+wget https://github.com/Mirantis/cri-dockerd/releases/download/v${VER}/cri-dockerd-${VER}.amd64.tgz
+tar xvf cri-dockerd-${VER}.amd64.tgz
+sudo mv cri-dockerd/cri-dockerd /usr/local/bin/
+
+# cri-docker Version Check
+cri-dockerd --version
+
+wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.service
+wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.socket
+sudo mv cri-docker.socket cri-docker.service /etc/systemd/system/
+sudo sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable cri-docker.service
+sudo systemctl enable --now cri-docker.socket
+
+# cri-docker Active Check
+sudo systemctl restart docker && sudo systemctl restart cri-docker
+sudo systemctl status cri-docker.socket --no-pager 
+
+# Docker cgroup Change Require to Systemd
+sudo mkdir /etc/docker
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+sudo systemctl restart docker && sudo systemctl restart cri-docker
+sudo docker info | grep Cgroup
+
+# Kernel Forwarding 
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+sudo sysctl --system
--- minikube_setup.sh
+++ minikube_setup.sh
@@ -1,33 +1,33 @@
-# minikube start --cpus 18 --memory 20000 --gpus all
+minikube start --driver docker --gpus all --cpus 18 --memory 20000  
 
-# minikube addons enable storage-provisioner-rancher
+minikube addons enable storage-provisioner-rancher
 
-# # Enable ingress-nginx
-# minikube addons enable ingress
+# Enable ingress-nginx
+minikube addons enable ingress
 
-# # Enable MetalLB in Minikube
-# minikube addons enable metallb
+# Enable MetalLB in Minikube
+minikube addons enable metallb
 
-# # Configure MetalLB with an IP range
-# kubectl apply -f - <<EOF
-# apiVersion: v1
-# kind: ConfigMap
-# metadata:
-#   namespace: metallb-system
-#   name: config
-# data:
-#   config: |
-#     address-pools:
-#     - name: default
-#       protocol: layer2
-#       addresses:
-#       - 192.168.49.100-192.168.49.200
-# EOF
+# Configure MetalLB with an IP range
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  namespace: metallb-system
+  name: config
+data:
+  config: |
+    address-pools:
+    - name: default
+      protocol: layer2
+      addresses:
+      - 192.168.49.100-192.168.49.200
+EOF
 
-# # Update ingress-nginx-controller to type LoadBalancer
-# kubectl patch svc ingress-nginx-controller -n ingress-nginx -p '{"spec": {"type": "LoadBalancer"}}'
+# Update ingress-nginx-controller to type LoadBalancer
+kubectl patch svc ingress-nginx-controller -n ingress-nginx -p '{"spec": {"type": "LoadBalancer"}}'
 
-# minikube addons enable dashboard
+minikube addons enable dashboard
 
 source ./mlflow/setup.sh
 source ./monitoring/setup.sh
